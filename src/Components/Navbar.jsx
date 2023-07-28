0dc7a28099de3d0800280c6e8620d56e931953a5
Philip Yi <philip.yi@duke.edu>
2023-07-28 14:00:34 -0500

    initial commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-[100px] flex justify-center items-center px-4 bg-[#060e1b] text-gray-300'>
+        <div>
+            hi
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
